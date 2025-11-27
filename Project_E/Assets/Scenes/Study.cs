fee4010e07e2dda5c8c6fae8ed2422df7ff3434a
Inyoung_Park <inyoung7682@gmail.com>
2025-11-27 17:38:45 +0900

    251127 수업

--- Project_E/Assets/Scenes/Study.cs
+++ Project_E/Assets/Scenes/Study.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class Study : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string say = "";
+    public char text = 'a';
+    public int var01 = 1;
+    public float var02 = 0.4f;
+    public double var03 = 0.5;
+    public bool var04 = true;
+
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log(this.say);
+        Debug.Log(this.text);
     }
 
     // Update is called once per frame
